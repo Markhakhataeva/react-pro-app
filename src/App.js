f81182c80447dc7ded8f5752e6c1f1dc44a74b64
mike <mike@hk>
2024-05-29 23:08:13 +0300

    first com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useEffect, useState} from "react";
+
+export const App = () => {
+  const[data,setData]=useState([])
+    const[loading,setLoading]=useState(true)
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((responce)=>{
+          if(!responce.ok){
+            throw new Error("error")
+          }
+          return responce.json()
+        })
+        .then((json)=>{
+          setData(json)
+            setLoading(false)
+
+        }).catch((e)=>{
+          console.log(e)
+    })
+  },[])
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Список дел</h1>
+          <>
+            {
+                loading ?(
+                    <div className="loading">.....</div>
+                ): data.map((item)=>{
+                    return(
+                        <ul>
+                            <li>
+                                <p>
+                                    <div key={item.id}>
+                                        {item.title}
+                                    </div>
+                                </p>
+                            </li>
+                        </ul>
+                    )
+                })
+
+
+            }
+          </>
     </div>
   );
 }
-
-export default App;
